35bf26f88c7e633d44f6153d324af79ea0e3afbc
ketimporta1 <patriciobc23@gmail.com>
2022-12-01 20:27:02 -0600

    .

--- src/components/Social.js
+++ src/components/Social.js
@@ -3,9 +3,11 @@ const Social = () => {
         <div className="social">
             <h2>Socials</h2>
 
-            <a href="" className="button">patriciobc23@gmail.com</a>
+            <a href="mailto:patriciobc23@gmail.com" className="button">patriciobc23@gmail.com</a>
             
             <a href="https://github.com/putricio01" className="button">GitHub</a>
+
+            <a href="https://nameless-mud-0239.on.fleek.co/" className="button">more about me</a>
         </div>
     );
 }
